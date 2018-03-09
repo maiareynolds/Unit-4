7fbb6890001a8c0e9f3a25cea3813596a075b14c
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-09 08:48:05 -0500

    Updated from Brython Server: 3/9/2018 8:48:04 AM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -1,1 +1,16 @@
+#Maia Reynolds
+#3/9/18
+#colorChangeWindow.py - random color by making function
 
+from ggame import *
+red=Color(0xFF0000,1)
+r=RectangleAsset(800,800,LineStyle(1,red),red)
+Sprite(r)
+def colorChange():
+    r=RectangleAsset(800,800,LineStyle(1,red),red)
+    Sprite(r)
+
+App().listenMouseEvent("click",mouseClick)
+App.run()
+
+colorChange()
