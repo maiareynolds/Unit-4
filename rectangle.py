c3272811055b6a8fbdf101656d230501ffa7d61e
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-09 08:35:03 -0500

    Updated from Brython Server: 3/9/2018 8:35:02 AM

--- rectangle.py
+++ rectangle.py
@@ -1,1 +1,8 @@
+#Maia Reynolds
+#3/9/18
+#rectangle.py - perimeter and area by making function
 
+def rectangle(length,width):
+    print("The area is ",(length*width))
+    print("The perimeter is ",2*length+2*width)
+rectangle(1,1)
