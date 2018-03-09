7d2cdf9a74517a807a6d523902c76a34f8fd512e
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-09 08:27:51 -0500

    Updated from Brython Server: 3/9/2018 8:27:50 AM

--- functionDemo.py
+++ functionDemo.py
@@ -2,6 +2,23 @@
 #3/9/18
 #FunctionDemo.py - how to write our own functions
 
+"""
 def hw():
     print("hello world")
-hw
+hw()#test
+""" """
+def double(thing2double):
+    print(thing2double*2)
+double("h")#test - can do numbers, strings, etc.
+""""""
+def bigger(a,b):
+    if a>b:
+        print(a)
+    else:
+        print(b)
+bigger(4,5)#test, can do with strings
+""""""
+def slope(x1,y1,x2,y2):
+    print((y2-y1)/(x2-x1))
+slope(0,0,2,2)#test
+"""
