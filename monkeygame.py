f99e9f429fe3388db9ffaf9108a1f0ef4e6ac130
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-22 08:49:03 -0400

    Updated from Brython Server: 3/22/2018 8:49:01 AM

--- monkeygame.py
+++ monkeygame.py
@@ -2,29 +2,58 @@
 #3/22/18
 #monkeygame.py - best monkey game ever
 
-
 from ggame import *
+from random import randint
 
-Rows=20
-Cols=35
-cellsize=60
+Rows=13
+Cols=25
+cellsize=40
 
 def moveRight(event):
-    monkey.x+=cellsize
+    if monkey.x<(Cols-1)*cellsize:
+        monkey.x+=cellsize
+        if monkey.x==banana.x and monkey.y==banana.y:
+            moveBanana()
+            data["score"]+=10
+            print(data["score"])
 def moveLeft(event):
-    monkey.x-=cellsize
+    if monkey.x>0:
+        monkey.x-=cellsize
+        if monkey.x==banana.x and monkey.y==banana.y:
+            moveBanana()
+            data["score"]+=10
+            print(data["score"])
 def moveUp(event):
-    monkey.y-=cellsize
+    if monkey.y>0:
+        monkey.y-=cellsize
+        if monkey.x==banana.x and monkey.y==banana.y:
+            moveBanana()
+            data["score"]+=10
+            print(data["score"])
 def moveDown(event):
-    monkey.y+=cellsize
+    if monkey.y<(Rows-1)*cellsize:
+        monkey.y+=cellsize
+        if monkey.x==banana.x and monkey.y==banana.y:
+            moveBanana()
+            data["score"]+=10
+            print(data["score"])
+def moveBanana():
+    banana.x=randint(0,Cols-1)*cellsize
+    banana.y=randint(0,Rows-1)*cellsize
+
 
 if __name__=="__main__":
+    data={}
+    data["score"]=0
     green=Color(0x006600,1)
     brown=Color(0x8B4513,1)
+    yellow=Color(0xFFFF00,1)
     jungleBox=RectangleAsset(cellsize*Cols,cellsize*Rows,LineStyle(1,green),green)
     monkeyBox=RectangleAsset(cellsize,cellsize,LineStyle(1,brown),brown)
+    bananaBox=RectangleAsset(cellsize,cellsize,LineStyle(1,yellow),yellow)
     Sprite(jungleBox)
     monkey=Sprite(monkeyBox)
+    banana=Sprite(bananaBox,(480,200))
     App().listenKeyEvent("keydown","right arrow",moveRight)
     App().listenKeyEvent("keydown","left arrow",moveLeft)
     App().listenKeyEvent("keydown","up arrow",moveUp)
