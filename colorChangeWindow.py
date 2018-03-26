30bba55f42553d1aa2078cdef8e4bd6a17470cf6
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-26 08:48:13 -0400

    Updated from Brython Server: 3/26/2018 8:48:11 AM

--- colorChangeWindow.py
+++ colorChangeWindow.py
@@ -3,14 +3,22 @@
 #colorChangeWindow.py - random color by making function
 
 from ggame import *
+from random import randint
 red=Color(0xFF0000,1)
-r=RectangleAsset(800,800,LineStyle(1,red),red)
-Sprite(r)
-def colorChange():
-    r=RectangleAsset(800,800,LineStyle(1,red),red)
-    Sprite(r)
+blue=Color(0x00FF00,1)
+green=Color(0x0000FF,1)
+r=RectangleAsset(1200,800,LineStyle(1,red),red)
+b=RectangleAsset(1200,800,LineStyle(1,blue),blue)
+g=RectangleAsset(1200,800,LineStyle(1,green),green)
+
+def mouseClick(event):
+    n=randint(1,3)
+    if n==1:
+        Sprite(r)
+    elif n==2:
+        Sprite(b)
+    else:
+        Sprite(g)
 
 App().listenMouseEvent("click",mouseClick)
-App.run()
-
-colorChange()
+App().run()
