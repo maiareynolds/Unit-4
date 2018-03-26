40114194681bed084fe80c52aa47c4c20744b783
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-26 08:28:38 -0400

    Updated from Brython Server: 3/26/2018 8:28:37 AM

--- warmUp11.py
+++ warmUp11.py
@@ -1,1 +1,13 @@
+#Maia Reynolds
+#3/26/18
+#warmUp11.py - returns true if = prime and false otherwise
 
+def prime(n):
+    for i in range(2,n):
+        if n%i==0:
+            return False
+    return True
+
+print(prime(7))
+print(prime(8))
+print(prime(78070))
