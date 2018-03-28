6c039320722cb31d6c00b918684b66f2f44b52d6
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-28 08:24:11 -0400

    Updated from Brython Server: 3/28/2018 8:24:10 AM

--- printSquares.py
+++ printSquares.py
@@ -1,1 +1,13 @@
+#Maia Reynolds
+#3/28/18
+#printSquares.py
 
+def squares(columns,rows):
+    print("+"+"--+"*columns)
+    r=0
+    while r<rows:
+        print("|"+"  |"*columns)
+        print("+"+"--+"*columns)
+        r+=1
+
+squares(2,4)
