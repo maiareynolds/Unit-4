c580d43f766755653c3820c05f2b4c83978e0cbc
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-28 08:40:37 -0400

    Updated from Brython Server: 3/28/2018 8:40:36 AM

--- stringUnion.py
+++ stringUnion.py
@@ -1,1 +1,15 @@
+#Maia Reynolds
+#3/28/18
+#stringUnion.py - two strings to non-repeating characters
 
+def stringUnion(word1,word2):
+    final=""
+    for ch in word1:
+        if ch not in final:
+            final+=ch
+    for ch in word2:
+        if ch not in final:
+            final+=ch
+    print(final)
+
+stringUnion("mississipe","mauiadbaoysifybsdiofbyiosdzvgbgz")
