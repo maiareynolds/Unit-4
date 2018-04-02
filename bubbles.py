7a3e596d54ff70dc18279e92b8a6bbd67063c893
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-01 22:26:09 -0400

    Updated from Brython Server: 4/1/2018 10:26:08 PM

--- bubbles.py
+++ bubbles.py
@@ -3,9 +3,20 @@
 #bubbles.py - creates random bubbles
 
 from random import randint
-red=Color(0xCD2626,1)
+lred=Color(0xFF4040,1)
+mred=Color(0xCD2626,1)
+dred=Color(0xB22222,1)
 lblue=Color(0x00FFFF,1)
-mblue=Color(0x009ACD,
+mblue=Color(0x009ACD,1)
+dblue=Color(0x483D8B,1)
+yellow=Color(0xFFFF00,1)
+lgreen=Color(0x00FF00,1)
+dgreen=Color(0x228B22,1)
+mgreen=Color(0x00C957,1)
+llgreen=Color(0x00FF7F,1)
+purple=Color(0x8E388E,1)
+dpurple=Color(0x800080,1)
+lpurple=Color(0x9B30FF,1)
 def click(event):
     r=randint(10,200)
     x=randint(0,800)
