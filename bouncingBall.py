b45468496d46540aaa32a3778d653b0b0f8a46c7
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-01 23:16:57 -0400

    Updated from Brython Server: 4/1/2018 11:16:56 PM

--- bouncingBall.py
+++ bouncingBall.py
@@ -9,20 +9,21 @@ rows=25
 cols=13
 ball=CircleAsset(cellsize,LineStyle(1,Color(0x1874CD,1)),Color(0x1874CD,1))
 
+ball.x=0
+ball.y=0
 def move():
-    ball.x=350
-    ball.y=250
-    ball.x+=1
-    ball.y+=1
-    Sprite(ball,(ball.x,ball.y))
-    if ball.x==0:
+    while ball.x<=800 and ball.y<=600:
         ball.x+=1
-    if ball.x==800:
-        ball.x-=1
-    if ball.y==0:
         ball.y+=1
-    if ball.y=600:
-        ball.y-=600
+        Sprite(ball,(ball.x,ball.y))
+        if ball.x==0:
+            ball.x+=1
+        if ball.y==0:
+            ball.y+=1
+        if ball.y==600:
+            ball.y-=600
+        if ball.x==800:
+            ball.x-=1
 
 move()
 
