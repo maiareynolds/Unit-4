5b695f9851de123a2765c737f54e1107024655f1
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-01 22:20:12 -0400

    Updated from Brython Server: 4/1/2018 10:20:11 PM

--- bubbles.py
+++ bubbles.py
@@ -1,1 +1,17 @@
+#Maia Reynolds
+#4/1/18
+#bubbles.py - creates random bubbles
 
+from random import randint
+red=Color(0xCD2626,1)
+lblue=Color(0x00FFFF,1)
+mblue=Color(0x009ACD,
+def click(event):
+    r=randint(10,200)
+    x=randint(0,800)
+    y=randint(0,700)
+    
+
+
+App().listenMouseEvent("click",click)
+App().run()
