f93eb1b6a9ec39e13c2d9d138e9355bdb9f98144
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-01 23:37:08 -0400

    Updated from Brython Server: 4/1/2018 11:37:07 PM

--- sunrise.py
+++ sunrise.py
@@ -1,1 +1,17 @@
+#Maia Reynolds
+#4/1/18
+#sunrise.py - makes sunrise
 
+from ggame import *
+
+def sunmove():
+    x=400
+    y=700
+    sun=CircleAsset(40,LineStyle(1,Color(0xFFFF00,1)),Color(0xFFFF00,1))
+    while y>0:
+        y-=1
+        sun.destroy()
+        Sprite(sun,(x,y))
+
+sunmove()
+App().run()
