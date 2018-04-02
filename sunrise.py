94a2feaf5bc615d5e4ebda3277e39185c34c69d8
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-04-01 23:39:34 -0400

    Updated from Brython Server: 4/1/2018 11:39:33 PM

--- sunrise.py
+++ sunrise.py
@@ -8,10 +8,11 @@ def sunmove():
     x=400
     y=700
     sun=CircleAsset(40,LineStyle(1,Color(0xFFFF00,1)),Color(0xFFFF00,1))
+    sun1=Sprite(sun,(x,y))
     while y>0:
         y-=1
-        sun.destroy()
-        Sprite(sun,(x,y))
+        sun1.destroy()
+        sun1=Sprite(sun,(x,y))
 
 sunmove()
 App().run()
